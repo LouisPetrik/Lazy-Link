fea981b44f978aae8c0c141d1e26c9a3312f57e9
Louis <33809479+LouisPetrik@users.noreply.github.com>
2020-10-20 15:35:41 +0200

    basic lazy loading for each specified link done - back buttons needs to work propery

--- static/app.js
+++ static/app.js
@@ -1,7 +1,14 @@
+// was noch rein muss: ein globales objekt für jeden lazy-link und
+// dessen entsprechende seite, deren html gefetcht wurde.
+
+// { linkIndex: 0, htmlContent: ''} objekt wo alle links und content gespeichert sind.
+let pages = []
+
 let aboutPage
 
 // die eigentliche funktion zum getten des codes
-function preload(route) {
+function preload(linkIndex, route) {
+	console.log('ID: ', linkIndex, route)
 	fetch(`${route}`)
 		.then(function (response) {
 			// The API call was successful!
@@ -9,7 +16,8 @@ function preload(route) {
 		})
 		.then(function (html) {
 			// This is the HTML from our response as a text string
-			aboutPage = html
+
+			pages.push({ linkIndex: linkIndex, htmlContent: html })
 		})
 		.then(function () {})
 		.catch(function (err) {
@@ -22,22 +30,39 @@ function preload(route) {
 
 // damit kann man das href-attribut vom ersten link getten:
 // document.getElementsByTagName("a")[0].getAttribute("href")
+
+let testVar = ''
+
 let listOfLazyLinks = []
 const linkEl = document.getElementsByTagName('a')
 for (let i = 0; i < linkEl.length; i++) {
 	if (linkEl[i].getAttribute('lazylink') !== null) {
+		// hier sollten wir den linkID und route getten und in die preload funktion passen
+		preload(i, linkEl[i].getAttribute('href'))
+
 		console.log('Should lazy:', linkEl[i])
+
 		// das verlassen der seite verhindern:
 		linkEl[i].addEventListener('click', (event) => {
+			console.log('if in for')
 			// when clicking on the link, to avoid the loading:
 			event.preventDefault()
 			console.log(linkEl[i], ' was pressed')
 			// changes the URL without reloading:s
 			window.history.pushState('', '', linkEl[i].getAttribute('href'))
 
-			document.write(aboutPage)
+			// get the needed page from the pags object:
+			console.log('pages in event', pages)
+			let correctPage = pages.filter((obj) => {
+				return obj.linkIndex === i
+			})
+
+			// re-write the page with the fetched html from the fitting page
+			document.write(correctPage[0].htmlContent)
 		})
 	}
 }
 
-preload('about')
+window.onhashchange = function () {
+	console.log('somethings wrong i can feel it')
+}
